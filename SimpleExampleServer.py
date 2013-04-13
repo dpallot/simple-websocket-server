4cd004120dd66a45e8f2da867c62a5f24a39a35c
opiate <dave.pallot@curtin.edu.au>
2013-04-14 01:45:07 +0800

    first commit

---
+++ SimpleExampleServer.py
@@ -0,0 +1,87 @@
+import signal, sys, ssl
+from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWebSocketServer
+from optparse import OptionParser
+
+
+class SimpleEcho(WebSocket):
+	def __init__(self, server, sock, address):
+		WebSocket.__init__(self, server, sock, address)
+
+	def handleMessage(self):
+		if self.data == None:
+			self.data = ''
+		
+		try:
+			self.sendMessage(str(self.data))
+		except Exception as n:
+			print n
+			
+	def handleConnected(self):
+		print self.address, 'connected'
+
+	def handleClose(self):
+		print self.address, 'closed'
+
+
+class SimpleChat(WebSocket):
+	def __init__(self, server, sock, address):
+		WebSocket.__init__(self, server, sock, address)
+
+	def handleMessage(self):
+		if self.data == None:
+			self.data = ''
+		
+		try:
+			for client in self.server.connections.itervalues():
+				if client != self:
+					client.sendMessage(str(self.address[0]) + ' - ' + str(self.data))
+		except Exception as n:
+			print n
+
+
+	def handleConnected(self):
+		print self.address, 'connected'
+		for client in self.server.connections.itervalues():
+			if client != self:
+				try:
+					client.sendMessage(str(self.address[0]) + ' - connected')
+				except Exception as n:
+					print n
+
+	def handleClose(self):
+		print self.address, 'closed'
+		for client in self.server.connections.itervalues():
+			if client != self:
+				try:
+					client.sendMessage(str(self.address[0]) + ' - disconnected')
+				except Exception as n:
+					print n
+
+
+if __name__ == "__main__":
+
+	parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
+	parser.add_option("--host", default='', type='string', action="store", dest="host", help="hostname (localhost)")
+	parser.add_option("--port", default=8000, type='int', action="store", dest="port", help="port (8000)")
+	parser.add_option("--example", default='echo', type='string', action="store", dest="example", help="echo, chat")
+	parser.add_option("--ssl", default=0, type='int', action="store", dest="ssl", help="ssl (1: on, 0: off (default))")
+	parser.add_option("--cert", default='./cert.pem', type='string', action="store", dest="cert", help="cert (./cert.pem)")
+	
+	(options, args) = parser.parse_args()
+
+	cls = SimpleEcho
+	if options.example == 'chat':
+		cls = SimpleChat	
+
+	if options.ssl == 1:
+		server = SimpleSSLWebSocketServer(options.host, options.port, cls, './cert.pem', './cert.pem')
+	else:	
+		server = SimpleWebSocketServer(options.host, options.port, cls)
+
+	def close_sig_handler(signal, frame):
+		server.close()
+		sys.exit()
+
+	signal.signal(signal.SIGINT, close_sig_handler)
+
+	server.serveforever()
