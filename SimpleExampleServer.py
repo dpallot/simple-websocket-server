35e5859312f4f3fa8bcce2a5555942da9f0392fb
opiate <opiate@github>
2014-08-22 22:21:51 +0800

    On request removed tabs for spaces Added license to top of each file

--- SimpleExampleServer.py
+++ SimpleExampleServer.py
@@ -1,3 +1,15 @@
+'''
+The MIT License (MIT)
+
+Copyright (c) 2013 Dave P.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
 import signal, sys, ssl, logging
 from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWebSocketServer
 from optparse import OptionParser
@@ -6,80 +18,80 @@ logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 class SimpleEcho(WebSocket):
 
-	def handleMessage(self):
-		if self.data is None:
-			self.data = ''
-		
-		try:
-			self.sendMessage(str(self.data))
-		except Exception as n:
-			print n
-			
-	def handleConnected(self):
-		print self.address, 'connected'
+   def handleMessage(self):
+      if self.data is None:
+         self.data = ''
+      
+      try:
+         self.sendMessage(str(self.data))
+      except Exception as n:
+         print n
+         
+   def handleConnected(self):
+      print self.address, 'connected'
 
-	def handleClose(self):
-		print self.address, 'closed'
+   def handleClose(self):
+      print self.address, 'closed'
 
 
 class SimpleChat(WebSocket):
 
-	def handleMessage(self):
-		if self.data is None:
-			self.data = ''
-		
-		for client in self.server.connections.itervalues():
-			if client != self:
-				try:
-					client.sendMessage(str(self.address[0]) + ' - ' + str(self.data))
-				except Exception as n:
-					print n
+   def handleMessage(self):
+      if self.data is None:
+         self.data = ''
+      
+      for client in self.server.connections.itervalues():
+         if client != self:
+            try:
+               client.sendMessage(str(self.address[0]) + ' - ' + str(self.data))
+            except Exception as n:
+               print n
 
 
-	def handleConnected(self):
-		print self.address, 'connected'
-		for client in self.server.connections.itervalues():
-			if client != self:
-				try:
-					client.sendMessage(str(self.address[0]) + ' - connected')
-				except Exception as n:
-					print n
+   def handleConnected(self):
+      print self.address, 'connected'
+      for client in self.server.connections.itervalues():
+         if client != self:
+            try:
+               client.sendMessage(str(self.address[0]) + ' - connected')
+            except Exception as n:
+               print n
 
-	def handleClose(self):
-		print self.address, 'closed'
-		for client in self.server.connections.itervalues():
-			if client != self:
-				try:
-					client.sendMessage(str(self.address[0]) + ' - disconnected')
-				except Exception as n:
-					print n
+   def handleClose(self):
+      print self.address, 'closed'
+      for client in self.server.connections.itervalues():
+         if client != self:
+            try:
+               client.sendMessage(str(self.address[0]) + ' - disconnected')
+            except Exception as n:
+               print n
 
 
 if __name__ == "__main__":
 
-	parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
-	parser.add_option("--host", default='', type='string', action="store", dest="host", help="hostname (localhost)")
-	parser.add_option("--port", default=8000, type='int', action="store", dest="port", help="port (8000)")
-	parser.add_option("--example", default='echo', type='string', action="store", dest="example", help="echo, chat")
-	parser.add_option("--ssl", default=0, type='int', action="store", dest="ssl", help="ssl (1: on, 0: off (default))")
-	parser.add_option("--cert", default='./cert.pem', type='string', action="store", dest="cert", help="cert (./cert.pem)")
-	parser.add_option("--ver", default=ssl.PROTOCOL_TLSv1, type=int, action="store", dest="ver", help="ssl version")
-	
-	(options, args) = parser.parse_args()
+   parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
+   parser.add_option("--host", default='', type='string', action="store", dest="host", help="hostname (localhost)")
+   parser.add_option("--port", default=8000, type='int', action="store", dest="port", help="port (8000)")
+   parser.add_option("--example", default='echo', type='string', action="store", dest="example", help="echo, chat")
+   parser.add_option("--ssl", default=0, type='int', action="store", dest="ssl", help="ssl (1: on, 0: off (default))")
+   parser.add_option("--cert", default='./cert.pem', type='string', action="store", dest="cert", help="cert (./cert.pem)")
+   parser.add_option("--ver", default=ssl.PROTOCOL_TLSv1, type=int, action="store", dest="ver", help="ssl version")
+   
+   (options, args) = parser.parse_args()
 
-	cls = SimpleEcho
-	if options.example == 'chat':
-		cls = SimpleChat	
+   cls = SimpleEcho
+   if options.example == 'chat':
+      cls = SimpleChat	
 
-	if options.ssl == 1:
-		server = SimpleSSLWebSocketServer(options.host, options.port, cls, options.cert, options.cert, version=options.ver)
-	else:	
-		server = SimpleWebSocketServer(options.host, options.port, cls)
+   if options.ssl == 1:
+      server = SimpleSSLWebSocketServer(options.host, options.port, cls, options.cert, options.cert, version=options.ver)
+   else:	
+      server = SimpleWebSocketServer(options.host, options.port, cls)
 
-	def close_sig_handler(signal, frame):
-		server.close()
-		sys.exit()
+   def close_sig_handler(signal, frame):
+      server.close()
+      sys.exit()
 
-	signal.signal(signal.SIGINT, close_sig_handler)
+   signal.signal(signal.SIGINT, close_sig_handler)
 
-	server.serveforever()
+   server.serveforever()
