864c6a4466bdc27d46a5164e4d448d3cefab4ed0
Andrew Click-Horn <andrewphorn@gmail.com>
2015-06-18 16:23:38 -0700

    Add setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='SimpleWebSocketServer',
+    version='1.0',
+    author='dpallot',
+    author_email='',
+    description='Simple python implementation of a websocket server.',
+    url='https://github.com/dpallot/simple-websocket-server/',
+    py_modules=['SimpleWebSocketServer'],
+    install_requires=[],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python'
+    ]
+)
