d49f2e6aafc53f591e7f4547da4c7311b2058ebb
Dave <dpallot@users.noreply.github.com>
2019-02-07 10:51:53 +0800

    Update SimpleHTTPSServer.py
    Add __main__ and clean up imports for python2 and 3

--- SimpleWebSocketServer/SimpleHTTPSServer.py
+++ SimpleWebSocketServer/SimpleHTTPSServer.py
@@ -3,10 +3,21 @@ The MIT License (MIT)
 Copyright (c) 2013 Dave P.
 '''
 
-import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
-# openssl req -new -x509 -days 365 -nodes -out cert.pem -keyout cert.pem
-httpd = BaseHTTPServer.HTTPServer(('', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile='./cert.pem', keyfile='./cert.pem', ssl_version=ssl.PROTOCOL_TLSv1)
-httpd.serve_forever()
+try:
+    from BaseHTTPServer import HTTPServer
+except:
+    from http.server import HTTPServer
+
+try:
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+except:
+    from http.server import SimpleHTTPRequestHandler
+
+if __name__ == "__main__":
+  # openssl req -new -x509 -days 365 -nodes -out cert.pem -keyout key.pem
+  httpd = BaseHTTPServer.HTTPServer(('', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+  httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile='./cert.pem', 
+                                 keyfile='./key.pem', ssl_version=ssl.PROTOCOL_TLSv1)
+  httpd.serve_forever()
