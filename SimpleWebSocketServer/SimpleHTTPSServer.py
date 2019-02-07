4a9defa176919142d4bf738d307fb77fea0a04ad
Dave <dpallot@users.noreply.github.com>
2019-02-07 10:53:05 +0800

    Update SimpleHTTPSServer.py
    Removed BaseHTTPServer, SimpleHTTPServer

--- SimpleWebSocketServer/SimpleHTTPSServer.py
+++ SimpleWebSocketServer/SimpleHTTPSServer.py
@@ -16,8 +16,8 @@ except:
     from http.server import SimpleHTTPRequestHandler
 
 if __name__ == "__main__":
-  # openssl req -new -x509 -days 365 -nodes -out cert.pem -keyout key.pem
-  httpd = BaseHTTPServer.HTTPServer(('', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-  httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile='./cert.pem', 
+    # openssl req -new -x509 -days 365 -nodes -out cert.pem -keyout key.pem
+    httpd = HTTPServer(('', 443), SimpleHTTPRequestHandler)
+    httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile='./cert.pem', 
                                  keyfile='./key.pem', ssl_version=ssl.PROTOCOL_TLSv1)
-  httpd.serve_forever()
+    httpd.serve_forever()
